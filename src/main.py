072dcd1844df2039765c7621426c223e19c0b91d
mathijscusters22 <mathijscusters@gmail.com>
2024-10-12 10:57:10 +0200

    fix!: Minio startup + video processing

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 import dotenv
-
+import os
 from util import process_videos
 from yolov5 import train, detect
+import logging
 
 from data.db.main import MinioBucketWrapper
 
-# TODO:
-# - Get Minio started by entering password
-# - Find videos and images to get frames and export them
-# - Check entire codebase
+# TODO: Find videos and images to get frames and export them
+# TODO: Test our system
 if __name__ == '__main__':
+    logging.basicConfig(filename='../log/app.log', level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
     dotenv.load_dotenv()
 
-    minio_url = dotenv.get_key("MINIO_URL")
-    minio_user = dotenv.get_key("MINIO_USER")
-    minio_password = dotenv.get_key("MINIO_PASSWORD")
-    minio_bucket_name = dotenv.get_key("MINIO_BUCKET_NAME")
+    minio_url = os.getenv("MINIO_URL")
+    minio_user = os.getenv("MINIO_USER")
+    minio_password = os.getenv("MINIO_PASSWORD")
+    minio_bucket_name = os.getenv("MINIO_BUCKET_NAME")
 
     client = MinioBucketWrapper(
         minio_url,
@@ -24,25 +26,32 @@ if __name__ == '__main__':
         minio_bucket_name
     )
 
+    vid = '../out/dataset/videos'
     out = '../out/dataset/frames'
     data = './data/data.yaml'
 
-    glob = client.list_obj()
-    videos = [client.get_obj(v) for v in glob]
+    # for v in os.listdir(put):
+    #     client.put_obj(v, put + '/' + v)
 
-    process_videos(videos, out, frame_rate=3)
+    if not os.listdir(vid):
+        videos = [client.get_obj(o)
+                  for o in client.list_obj()]
+    else:
+        videos = os.listdir(vid)
 
-    train.run(
-        data_yaml=data,
-        img_size=640,
-        batch_size=16,
-        epochs=100,
-        weights='yolov5s.pt'
-    )
+    process_videos(videos, out, frame_rate=5)
 
-    detect.run(
-        video_source='test_video.mp4',
-        weights_path='runs/train/exp/weights/best.pt',
-        img_size=640,
-        output_dir='runs/detect'
-    )
+    # train.run(
+    #     data_yaml=data,
+    #     img_size=640,
+    #     batch_size=16,
+    #     epochs=50,
+    #     weights='yolov5s.pt'
+    # )
+    #
+    # detect.run(
+    #     video_source=test,
+    #     weights_path='runs/train/exp/weights/best.pt',
+    #     img_size=640,
+    #     output_dir='../datasets/detect'
+    # )
