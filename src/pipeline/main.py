1b6c9cbceaa540cb9b0f6cad2e1cceb5f1472eed
mathijscusters22 <mathijscusters@gmail.com>
2024-12-08 20:13:53 +0100

    refactor: minor refactor in code

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import torch
 import yaml
+from torchvision.transforms import transforms
 from ultralytics import YOLO
 
 from src.models.classifiers.classifier import CNN
@@ -27,6 +28,13 @@ class Tracker:
         self.classifier = CNN(_config).to(_config['system']['device'])
         self.classifier.eval()
 
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+
         self.process_video()
 
     def _process_frame(self, frame, ctime):
@@ -37,78 +45,73 @@ class Tracker:
             if not result.boxes or result.boxes.id is None:
                 continue
 
-            for box, cls, track_id, conf in zip(
-                    result.boxes.xyxy.cpu().numpy(),
-                    result.boxes.cls.cpu().numpy(),
-                    result.boxes.id.cpu().numpy(),
-                    result.boxes.conf.cpu().numpy()
-            ):
-                track_id = int(track_id)
+            boxes = result.boxes.xyxy.cpu().numpy()
+            confidences = result.boxes.conf.cpu().numpy()
+            classes = result.boxes.cls.cpu().numpy()
 
-                if track_id not in self.history:
-                    self._reset(track_id)
+            indices = cv2.dnn.NMSBoxes(boxes.tolist(), confidences.tolist(),
+                                       score_threshold=0.5, nms_threshold=0.4)
 
-                self.history[track_id]['bbox'] = box
+            if len(indices) > 0:
+                for i in indices.flatten():  # type: ignore
+                    box = boxes[i]
+                    cls = classes[i]
+                    conf = confidences[i]
+                    track_id = int(result.boxes.id[i])
 
-                if self.history[track_id]['state'] == 'MONITORING' and int(cls) == 1:
-                    self.history[track_id]['state'] = 'FALL_DETECTED'
-                    self.dtime[track_id] = ctime
-
-                elif self.history[track_id]['state'] == 'FALL_DETECTED':
-                    if ctime - self.dtime[track_id] >= 2.0:
-                        self.history[track_id]['state'] = 'MOTION_TRACKING'
-                        self.mtime[track_id] = ctime
-                        self.last_pos[track_id] = box
-
-                elif self.history[track_id]['state'] == 'MOTION_TRACKING':
-                    elapsed = ctime - self.mtime[track_id]
-                    motion = self._detect_motion(frame, track_id)
-
-                    if motion:
+                    if track_id not in self.history:
                         self._reset(track_id)
-                    elif elapsed >= 4.0:
-                        with torch.no_grad():
-                            frame_tensor = self._preprocess_frame(frame, track_id)
-                            output = self.classifier(frame_tensor)
-                            emergency_prob = output.item()
 
-                        if emergency_prob <= 0.50:
-                            self.history[track_id]['state'] = 'EMERGENCY'
+                    self.history[track_id]['bbox'] = box
 
-                x1, y1, x2, y2 = box
+                    if self.history[track_id]['state'] == 'MONITORING' and int(cls) == 1:
+                        self.history[track_id]['state'] = 'FALL_DETECTED'
+                        self.dtime[track_id] = ctime
 
-                color = (0, 255, 0)
-                if self.history[track_id]['state'] == 'FALL_DETECTED':
-                    color = (0, 165, 255)
-                elif self.history[track_id]['state'] == 'MOTION_TRACKING':
-                    color = (0, 255, 255)
-                elif self.history[track_id]['state'] == 'EMERGENCY':
-                    color = (0, 0, 255)
+                    elif self.history[track_id]['state'] == 'FALL_DETECTED':
+                        if ctime - self.dtime[track_id] >= 2.0:
+                            self.history[track_id]['state'] = 'MOTION_TRACKING'
+                            self.mtime[track_id] = ctime
+                            self.last_pos[track_id] = box
 
-                cv2.rectangle(
-                    annotated_frame,
-                    (int(x1), int(y1)),
-                    (int(x2), int(y2)),
-                    color,
-                    2,
-                )
+                    elif self.history[track_id]['state'] == 'MOTION_TRACKING':
+                        elapsed = ctime - self.mtime[track_id]
+                        motion = self._detect_motion(frame, track_id)
 
-                label = f"ID: {track_id}, State: {self.history[track_id]['state']}"
-                label_position = (int(x1), int(y1) - 10)
-                cv2.putText(
-                    annotated_frame,
-                    label,
-                    label_position,
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    color,
-                    2,
-                    lineType=cv2.LINE_AA
-                )
-                print(
-                    f"Track ID {track_id}: State: {self.history[track_id]['state']}, BBox: {self.history[track_id]['bbox']}")
+                        if motion:
+                            self._reset(track_id)
+                        elif elapsed >= 3.0:
+                            with torch.no_grad():
+                                frame_tensor = self._preprocess_frame(frame, track_id)
+                                output = self.classifier(frame_tensor)
+                                emergency_prob = output.item()
+                            if emergency_prob <= 0.5:
+                                self.history[track_id]['state'] = 'EMERGENCY'
+
+                    annotated_frame = self._draw(annotated_frame, track_id, self._get_state_color(track_id))
 
         cv2.imshow("Bot Brigade", annotated_frame)
+
+    def _draw(self, frame, track_id, color):
+        x1, y1, x2, y2 = map(int, self.history[track_id]['bbox'])
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+
+        label = f"ID: {track_id}, State: {self.history[track_id]['state']}"
+        label_position = (int(x1), int(y1) - 10)
+
+        cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2, lineType=cv2.LINE_AA)
+
+        return frame
+
+    def _get_state_color(self, track_id):
+        state = self.history[track_id]['state']
+        if state == 'FALL_DETECTED':
+            return 0, 165, 255
+        elif state == 'MOTION_TRACKING':
+            return 0, 255, 255
+        elif state == 'EMERGENCY':
+            return 0, 0, 255
+        return 0, 255, 0
 
     def _reset(self, track_id):
         self.history[track_id] = {'state': 'MONITORING', 'bbox': None}
@@ -120,13 +123,8 @@ class Tracker:
     def _preprocess_frame(self, frame, track_id):
         x1, y1, x2, y2 = map(int, self.history[track_id]['bbox'])
         person_crop = frame[y1:y2, x1:x2]
-
-        crop_tensor = cv2.resize(person_crop, (128, 128))
-        crop_tensor = crop_tensor / 255.0
-        crop_tensor = torch.tensor(crop_tensor, dtype=torch.float32).permute(2, 0, 1)
-        crop_tensor = crop_tensor.unsqueeze(0).to('cuda')
-
-        return crop_tensor
+        frame_tensor = self.transform(person_crop).unsqueeze(0).to('cuda')
+        return frame_tensor
 
     def _detect_motion(self, frame, track_id):
         motion = False
@@ -136,12 +134,12 @@ class Tracker:
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
         if self.static_back[track_id] is None or self.static_back[track_id].shape != gray.shape:  # type: ignore
-            self.static_back[track_id] = gray
+            self.static_back[track_id] = gray  # type: ignore
             return motion
 
-        diff_frame = cv2.absdiff(self.static_back[track_id], gray)
+        diff_frame = cv2.absdiff(self.static_back[track_id], gray)  # type: ignore
         thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
-        thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)  # type: ignore
         cnts, _ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in cnts:
@@ -172,4 +170,4 @@ class Tracker:
 
 
 if __name__ == '__main__':
-    Tracker('../data/pipeline_eval_data/sudden cardiac arrest tatami.webm', '../../config.yaml', device='cuda')
+    Tracker('../data/pipeline_eval_data/simulation_chantier_2.mp4', '../../config.yaml')
