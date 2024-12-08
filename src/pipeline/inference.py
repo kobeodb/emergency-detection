8ac961b940757827dca1a5f612650b7dd9fcd465
kobeodb <113979782+kobeodb@users.noreply.github.com>
2024-12-08 22:24:14 +0100

    /

--- src/pipeline/inference.py
+++ src/pipeline/inference.py
@@ -1,13 +1,17 @@
 import time
+from collections import defaultdict
+
 import cv2
 import numpy as np
 import torch
 import yaml
+from jinja2.nodes import Continue
+from rich.progress import track
 from ultralytics import YOLO
+
 from src.models.classifiers.classifier import CNN
 
-
-class EmergencyDetection:
+class EmergencyDetector:
     def __init__(self, config_path, model_path):
         with open(config_path) as f:
             self.config = yaml.safe_load(f)
@@ -16,122 +20,181 @@ class EmergencyDetection:
 
         self.fall_detector = YOLO(self.config['model']['detector']['finetuned_weights_path'])
 
+        self.dtime = defaultdict(float)
+        self.mtime = defaultdict(float)
+
+        self.history = defaultdict(lambda: {'state': 'MONITORING', 'bbox': None})
+        self.last_pos = defaultdict(lambda: None)
+        self.static_back = defaultdict(lambda: None)
+
+        # input_size = self.config['model']['classifier']['input_size']
+        # self.classifier = make_model(trial=None, input_size=input_size).to(self.config['system']['device'])
+        # checkpoint = torch.load(model_path, map_location=self.config['system']['device'])
+        # self.classifier.load_state_dict(checkpoint['classifier_state_dict'])
+        # self.classifier.eval()
+
         self.classifier = CNN(self.config).to(self.device)
         checkpoint = torch.load(model_path, map_location=self.device)
         self.classifier.load_state_dict(checkpoint['classifier_state_dict'])
         self.classifier.eval()
 
-        self.detection_states = {}
-        self.motion_threshold = 50
 
-    def reset_detection(self, bbox):
-        """Reset the state for a specific detection."""
-        if bbox in self.detection_states:
-            del self.detection_states[bbox]
+    def reset(self, track_id):
+        self.history[track_id] = {'state': 'MONITORING', 'bbox': None}
+        self.dtime[track_id] = 0.0
+        self.mtime[track_id] = 0.0
+        self.last_pos[track_id] = None
+        self.static_back[track_id] = None
 
     def process_video(self, video_path):
         cap = cv2.VideoCapture(video_path)
         start_time = time.time()
 
         while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
+            success, frame = cap.read()
+
+            if not success:
                 break
 
-            current_time = time.time() - start_time
-            results = self.process_frame(frame, current_time)
+            ctime = time.time() - start_time
+            self.process_frame(frame, ctime)
 
-            self._visualize_frame(frame, results)
-
-            cv2.imshow('Emergency Detection Pipeline', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         cap.release()
         cv2.destroyAllWindows()
 
-    def process_frame(self, frame, current_time):
-        results = self.fall_detector(frame)
+    def process_frame(self, frame, ctime):
+        try:
+            if frame.shape[0] > 1080:
+                scale = 1080 / frame.shape[0]
+                width = int(frame.shape[1] * scale)
+                frame = cv2.resize(frame, (width, 1080))
 
-        detections = []
-        if len(results[0].boxes) > 0:
-            for box in results[0].boxes:
-                bbox = tuple(box.xyxy.cpu().numpy()[0])
-                confidence = box.conf.item()
-                fall_detected = box.cls.item() == 1
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                if confidence >= 0.5:
+            results = self.fall_detector.track(
+                frame_rgb,
+                persist=True,
+                tracker="bytetrack.yaml",
+                verbose=False,
+                conf=0.5,
+                iou=0.4,
+            )
+
+            annotated_frame = frame.copy()
+            detections = []
+
+            for result in results:
+                if not result.boxes or result.boxes.id is None:
+                    continue
+
+                for bbox, cls, track_id, conf in zip(
+                    result.boxes.xyxy.cpu().numpy(),
+                    result.boxes.cls.cpu().numpy(),
+                    result.boxes.id.cpu().numpy(),
+                    result.boxes.conf.cpu().numpy()
+            ):
+                    track_id = int(track_id)
+                    if track_id not in self.history:
+                        self.reset(track_id)
+
+                    self.history[track_id]['bbox'] = bbox
+                    state = self.history[track_id]['state']
+
+                    if state == 'MONITORING' and int(cls) == 1:
+                        self.history[track_id]['state'] = 'FALL_DETECTED'
+                        self.dtime[track_id] = ctime
+                        print(f"Fall detected for bbox {bbox} at {ctime:.2f}s with confidence: {conf}")
+
+                    elif state == 'FALL_DETECTED':
+                        fall_time = ctime - self.dtime[track_id]
+                        if fall_time >= 2.0:
+                            self.history[track_id]['state'] = 'MOTION_TRACKING'
+                            self.mtime[track_id] = ctime
+                            self.last_pos[track_id] = bbox
+                            print(f"Starting motion tracking for bbox {bbox} at {ctime:.2f}s")
+
+                    elif state == 'MOTION_TRACKING':
+                        elapsed = ctime - self.mtime[track_id]
+                        track_time = elapsed
+                        has_motion = self.detect_motion(frame, track_id)
+                        print(elapsed)
+
+                        if has_motion:
+                            self.reset(track_id)
+                            print(f"Motion detected for bbox {bbox} at {ctime:.2f}s")
+                        elif elapsed >= 4.0:
+                            with torch.no_grad():
+                                frame_tensor = self.preprocess_frame(frame, track_id)
+                                output = self.classifier(frame_tensor)
+                                emergency_prob = output.item()
+                                print(f"Emergency probability for bbox {bbox}: {emergency_prob:.2f}")
+
+                                if emergency_prob <= 0.5:
+                                    self.history[track_id]['state'] = 'EMERGENCY'
+                                    print(
+                                        f"Emergency confirmed for bbox {bbox} at {ctime:.2f}s with confidence {emergency_prob:.2f}")
+                                else:
+                                    print("no emergency for this person he okokokokok")
+                                    self.reset(track_id)
+
+
+                    x1, y1, x2, y2 = bbox
+                    color = (0, 255, 0)
+                    if self.history[track_id]['state'] == 'FALL_DETECTED':
+                        color = (0, 165, 255)
+                    elif self.history[track_id]['state'] == 'MOTION_TRACKING':
+                        color = (0, 255, 255)
+                    elif self.history[track_id]['state'] == 'EMERGENCY':
+                        color = (0, 0, 255)
+
+                    cv2.rectangle(
+                        annotated_frame,
+                        (int(x1), int(y1)),
+                        (int(x2), int(y2)),
+                        color,
+                        2,
+                    )
+
+                    label = f"ID: {track_id}, State: {self.history[track_id]['state']}"
+                    label_position = (int(x1), int(y1) - 10)
+                    cv2.putText(
+                        annotated_frame,
+                        label,
+                        label_position,
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        color,
+                        2,
+                        lineType=cv2.LINE_AA
+                    )
+
                     detections.append({
                         'bbox': bbox,
-                        'confidence': confidence,
-                        'fall_detected': fall_detected
+                        'class': int(cls),
+                        'track_id': track_id,
+                        'confidence': conf,
+                        'state': self.history[track_id]['state']
                     })
 
-        for bbox, state_data in self.detection_states.items():
-            print(f"BBOX: {bbox}, Current State: {state_data['state']}, Detection Data: {state_data}")
+                cv2.imshow("Bot Brigade", annotated_frame)
 
-        for detection in detections:
-            bbox = detection['bbox']
-            confidence = detection['confidence']
-            fall_detected = detection['fall_detected']
+            return {'detections': detections}
 
-            if bbox not in self.detection_states:
-                self.detection_states[bbox] = {
-                    'state': 'MONITORING',
-                    'fall_detection_time': None,
-                    'motion_tracking_start': None,
-                    'last_position': None
-                }
+        except cv2.error as e:
+            print(f"OpenCV error: {str(e)}")
+            self.fall_detector = YOLO(self.config['model']['detector']['finetuned_weights_path'])
+            return {'detections': []}
 
-            state_data = self.detection_states[bbox]
-            current_state = state_data['state']
+        except Exception as e:
+            print(f"Unexpected error: {str(e)}")
+            return {'detections': []}
 
-            if current_state == 'MONITORING':
-                if fall_detected:
-                    state_data['state'] = 'FALL_DETECTED'
-                    state_data['fall_detection_time'] = current_time
-                    print(f"Fall detected for bbox {bbox} at {current_time:.2f}s with confidence: {confidence}")
 
-            elif current_state == 'FALL_DETECTED':
-                if current_time - state_data['fall_detection_time'] >= 2.0:
-                    state_data['state'] = 'MOTION_TRACKING'
-                    state_data['motion_tracking_start'] = current_time
-                    state_data['last_position'] = bbox
-                    print(f"Starting motion tracking for bbox {bbox} at {current_time:.2f}s")
-
-            elif current_state == 'MOTION_TRACKING':
-                elapsed_time = current_time - state_data['motion_tracking_start']
-                has_motion = self.detect_motion(bbox, state_data)
-
-                if has_motion:
-                    self.reset_detection(bbox)
-                    print(f"Motion detected for bbox {bbox} at {current_time:.2f}s")
-                elif elapsed_time >= 4.0:
-                    self.classifier.eval()
-                    with torch.no_grad():
-                        frame_tensor = self.preprocess_frame(frame, bbox)
-                        output = self.classifier(frame_tensor)
-                        emergency_prob = output.item()
-                        print(f"Emergency probability for bbox {bbox}: {emergency_prob:.2f}")
-
-                    if emergency_prob <= 0.5:
-                        state_data['state'] = 'EMERGENCY'
-                        print(
-                            f"Emergency confirmed for bbox {bbox} at {current_time:.2f}s with confidence {emergency_prob:.2f}")
-                        return {
-                            'state': state_data['state'],
-                            'confidence': emergency_prob,
-                            'bbox': bbox
-                        }
-                    self.reset_detection(bbox)
-
-        return {
-            'states': self.detection_states,
-            'detections': detections
-        }
-
-    def preprocess_frame(self, frame, bbox):
-        x1, y1, x2, y2 = map(int, bbox)
+    def preprocess_frame(self, frame, track_id):
+        x1, y1, x2, y2 = map(int, self.history[track_id]['bbox'])
         person_crop = frame[y1:y2, x1:x2]
 
         crop_tensor = cv2.resize(person_crop, (
@@ -143,59 +206,30 @@ class EmergencyDetection:
 
         return crop_tensor
 
-    def detect_motion(self, current_bbox, state_data):
-        if state_data['last_position'] is None:
-            state_data['last_position'] = current_bbox
-            return True
+    def detect_motion(self, frame, track_id):
+        motion = False
+        x1, y1, x2, y2 = map(int, self.history[track_id]['bbox'])
 
-        current_center = [(current_bbox[0] + current_bbox[2]) / 2,
-                          (current_bbox[1] + current_bbox[3]) / 2]
-        last_center = [(state_data['last_position'][0] + state_data['last_position'][2]) / 2,
-                       (state_data['last_position'][1] + state_data['last_position'][3]) / 2]
+        gray = cv2.cvtColor(frame[y1:y2, x1:x2], cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        distance = np.sqrt((current_center[0] - last_center[0]) ** 2 +
-                           (current_center[1] - last_center[1]) ** 2)
+        if self.static_back[track_id] is None or self.static_back[track_id].shape != gray.shape:  # type: ignore
+            self.static_back[track_id] = gray
+            return motion
 
-        state_data['last_position'] = current_bbox
-        return distance > self.motion_threshold
+        diff_frame = cv2.absdiff(self.static_back[track_id], gray)
+        thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
+        cnts, _ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    def _visualize_frame(self, frame, results):
-        colors = {
-            'MONITORING': (0, 255, 0),  # Green
-            'FALL_DETECTED': (0, 165, 255),  # Orange
-            'MOTION_TRACKING': (0, 255, 255),  # Yellow
-            'EMERGENCY': (0, 0, 255)  # Red
-        }
+        for contour in cnts:
+            if cv2.contourArea(contour) < 10000:
+                continue
+            motion = True
 
-        for detection in results['detections']:
-            bbox = detection['bbox']
-            confidence = detection['confidence']
-            state = self.detection_states[bbox]['state']
-            color = colors[state]
-
-            x1, y1, x2, y2 = map(int, bbox)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 1)
-
-            cv2.putText(frame, f"State: {state}", (x1, y1 - 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-            cv2.putText(frame, f"Confidence: {confidence:.2f}", (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
-            if state == 'FALL_DETECTED':
-                fall_time = time.time() - self.detection_states[bbox]['fall_detection_time']
-                cv2.putText(frame, f"Wait Time: {fall_time:.1f}s", (x1, y1 - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
-            elif state == 'MOTION_TRACKING':
-                track_time = time.time() - self.detection_states[bbox]['motion_tracking_start']
-                cv2.putText(frame, f"Track Time: {track_time:.1f}s", (x1, y1 - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
-            elif state == 'EMERGENCY':
-                cv2.putText(frame, "EMERGENCY DETECTED", (x1, y1 - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+        return motion
 
 
 if __name__ == "__main__":
-    system = EmergencyDetection('../../config/config.yaml', '../models/classifiers/best_model.pth')
-    system.process_video("../data/pipeline_eval_data/test_videos/sudden cardiac arrest tatami.webm")
+    system = EmergencyDetector('../../config/config.yaml', '../models/classifiers/best_model.pth')
+    system.process_video("../data/pipeline_eval_data/test_videos/simulation_chantier.mp4")
